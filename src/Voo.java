ee2eb7e783adf048eec5f4881e85b715abaeea1e
vicfraga <victortelli@gmail.com>
2018-09-18 09:32:55 -0300

    Inicia exercicio voos

---
+++ src/Voo.java
@@ -0,0 +1,21 @@
+public class Voo {
+        public String origem;
+        public String destino;
+        public String equipamento;
+        public int duracao;
+
+
+    public Voo(String origem, String destino, String equipamento, int duracao) {
+    this.origem=origem;
+    this.destino=destino;
+    this.equipamento=equipamento;
+    this.duracao=duracao;
+
+
+    }
+    public String saindoDe(){
+
+    }
+    public String 
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        List <Voo> voos;
+        voos= new ArrayList<>();
+        Voo v1= new Voo("poa", "Arj", "boing 747", 190);
+        Voo v2= new Voo("asp", "Acr", "boing 457", 188);
+        Voo v3= new Voo("ale", "Afp", "boing 787", 145);
+
+
+
+        // write your code here
+    }
+}
